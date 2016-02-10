8d5f4e3d287fe011f486a0ff7a0880a96da38af5
David Forero <davefogo@gmail.com>
2016-02-09 23:04:17 -0500

    assignment 33 rails more crud: complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :questions
   resources :posts
   get 'about' => 'welcome#about'
-
   root 'welcome#index'
 end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'random_data'
+
+RSpec.describe Question, type: :model do
+  let(:question) {Question.create(title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false)}
+
+  describe " attributes " do
+    it "responds to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "responds to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "responds to resolved" do
+      expect(question).to respond_to(:resolved)
+    end
+  end
+end
