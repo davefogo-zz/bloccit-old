a92d1af55f0025be9c5672051b4bf1abf9af1240
David Forero <davefogo@gmail.com>
2016-02-12 09:07:12 -0500

    Assignment 34 rails topics and posts:complete

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Topic, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph )}
 
   it {is_expected.to have_many(:posts)}
+  it {is_expected.to have_many(:sponsored_posts)}
 
   describe "attributes" do
     it "responds to name" do
@@ -22,7 +23,5 @@ RSpec.describe Topic, type: :model do
     it "is public by default" do
       expect(topic.public).to be(true)
     end
-
-
   end
 end
