fd21e8fc99fdc8325f3b752a4ea0e17ba9df7e92
David Forero <davefogo@gmail.com>
2016-02-12 19:04:43 -0500

    Assignment 35 rails validation

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph )}
 
   it {is_expected.to have_many(:posts)}
+  it {is_expected.to validate_presence_of :name}
+  it {is_expected.to validate_presence_of :description}
+  it {is_expected.to validate_length_of(:name).is_at_least(5)}
+  it {is_expected.to validate_length_of(:description).is_at_least(15)}
 
   describe "attributes" do
     it "responds to name" do
@@ -22,7 +26,5 @@ RSpec.describe Topic, type: :model do
     it "is public by default" do
       expect(topic.public).to be(true)
     end
-
-
   end
 end
